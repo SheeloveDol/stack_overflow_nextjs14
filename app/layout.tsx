8d399bbca5252feaf2e4bb925c58cb54be933321
Sheelove Dol <siesel26@gmail.com>
2023-11-07 11:42:47 -0800

    Added assests

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,51 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react' 
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-space-grotesk',
+})
+
+export const metadata: Metadata = {
+  title: "StuckOverFlow",
+  description:
+    "StuckOverFlow is a Q&A site for professional and enthusiast programmers. It's built and run by you as part of the Stack Exchange network of Q&A sites. With your help, we're working together to build a library of detailed answers to every question about programming.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
- 
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
